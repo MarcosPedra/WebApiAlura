899a533411e86f43f4ea18751ebfd0648505ad3f
MarcosPedra <marcos.spedra@gmail.com>
2019-03-07 23:00:34 -0300

    PrimeiroCommit
    Tutorial de WebApi da Alura 2017

---
+++ LojaApiClient/LojaApiClient/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String conteudo;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:59803/api/carrinho/1");
+            request.Method = "POST";
+            request.Accept = "application/xml";
+
+            string json = "{'Produtos':[{'Id':6237,'Preco':2000.0,'Nome':'PSP','Quantidade':5}],'Endereco':'Rua Vergueiro 3185, 8 andar, Sao Paulo','Id':2}";
+            byte[] jsonBytes = Encoding.UTF8.GetBytes(json);
+            request.GetRequestStream().Write(jsonBytes, 0, jsonBytes.Length);
+
+            request.ContentType = "application/json";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Headers["Location"]);
+            Console.Read();
+        }
+
+        static void TestaPostXML()
+        {
+            String conteudo;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:59803/api/carrinho/1");
+            request.Method = "POST";
+            request.Accept = "application/xml";
+
+            string json = "{'Produtos':[{'Id':6237,'Preco':2000.0,'Nome':'PSP','Quantidade':5}],'Endereco':'Rua Vergueiro 3185, 8 andar, Sao Paulo','Id':2}";
+            byte[] jsonBytes = Encoding.UTF8.GetBytes(json);
+            request.GetRequestStream().Write(jsonBytes, 0, jsonBytes.Length);
+
+            request.ContentType = "application/json";
+
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                conteudo = reader.ReadToEnd();
+            }
+
+            Console.Write(conteudo);
+            Console.Read();
+        }
+
+        static void TestaGetXML()
+        {
+            String conteudo;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:59803/api/carrinho/1");
+            request.Method = "GET";
+            request.Accept = "application/xml";
+            //request.Accept = "application/json"; por padrão o webapi irá retornar json, posso retornar xml ou forçar json
+
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                conteudo = reader.ReadToEnd();
+            }
+
+            Console.Write(conteudo);
+            Console.Read();
+        }
+    }
+}
---
+++ LojaApiServer/LojaAPI/LojaAPI/Controllers/CarrinhoController.cs
@@ -0,0 +1,57 @@
+﻿using Loja.DAO;
+using Loja.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaAPI.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                CarrinhoDAO dao = new CarrinhoDAO();
+                Carrinho carrinho = dao.Busca(id);
+
+                return Request.CreateResponse(HttpStatusCode.OK, carrinho);
+            }
+            catch(KeyNotFoundException)
+            {
+                string mensagem = string.Format("O carrinho {0} não foi encontrado", id);
+                HttpError error = new HttpError(mensagem);
+                return Request.CreateResponse(HttpStatusCode.NotFound, error);
+            }
+
+        }
+
+        public HttpResponseMessage Post([FromBody] Carrinho carrinho)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            dao.Adiciona(carrinho);
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+
+            String location = Url.Link("DefaultApi", new { controller = "carrinho", id = carrinho.Id });
+            response.Headers.Location = new Uri(location);
+
+            return response;
+        }
+
+        public static void TestePost()
+        {
+            //BuscaCarrinho() por padrão usar o método GET
+            //public string Get(int id)
+            //return carrinho.ToXml();
+
+            CarrinhoDAO dao = new CarrinhoDAO();
+            //dao.Adiciona(carrinho);
+            //return "sucesso";
+        }
+
+    }
+}
